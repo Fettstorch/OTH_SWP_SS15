79d5f09220ff9c70f9057b9f98d830ebcef69f87
Fettstorch <matt3@gmx.net>
2015-03-25 20:46:54 +0100

    Temporäres Hinzufügen eines Dummy NUnit Testprojektes
    Solange die Skriptumgebung (vor allem im Labor) noch ein wenig wackelig
    auf den Beinen ist, würde ich gern ein Dummy NUnit Projekt einfügen.
    Zum einen ein Best-Practise mit NUnit bereitzustellen, als auch zum
    Testen der Skripts.
    
    -> Wird wieder entfernt sobald die korrekte Arbeitsweise der Skripte auf
    allen Rechnern gewährleistet ist.

---
+++ Basisverzeichnis/trunk/03_Implementierung/src/NUnitTestLib/NUnitTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnitTestLib;
+
+
+namespace NUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
