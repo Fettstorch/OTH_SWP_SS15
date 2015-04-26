2a03adc83db90de2079b4d0003aaf5803a4280f0
Coder-Luke <patrick.schiessl@googlemail.com>
2015-04-26 15:33:14 +0200

    Projekt des Prototyps für Graph zeichnen hinzugefügt

---
+++ Basisverzeichnis/trunk/09_Beispiele/Graph zeichnen/GraphDrawing/GraphDrawing/DrawingElements/KanteUc.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphDrawing.DrawingElements
+{
+    /// <summary>
+    /// Interaction logic for KanteUc.xaml
+    /// </summary>
+    public partial class KanteUc : UserControl
+    {
+        public KanteUc(KnotenUc source, KnotenUc dest)
+        {
+            InitializeComponent();
+            SourceKnotenUc = source;
+            DestKnotenUc = dest;
+            SourceKnotenUc.AddKante(this);
+            DestKnotenUc.AddKante(this);
+            if (source.Equals(dest))
+                Type = KantenType.Recursiv;
+            else if (source.Offset < dest.Offset)
+                Type = KantenType.Proecess;
+            else
+                Type = KantenType.ReturnProcess;
+       
+            Recalc(SourceKnotenUc);
+        }
+
+        public readonly KnotenUc SourceKnotenUc;
+
+        public readonly KnotenUc DestKnotenUc;
+
+
+        private PathGeometry pthGeometry = new PathGeometry();
+        private PathFigureCollection pthFigureCollection = new PathFigureCollection();
+        private PathSegmentCollection myPathSegmentCollection = new PathSegmentCollection();
+        private BezierSegment bzSeg = new BezierSegment();
+        private PathFigure pthFigure = new PathFigure();
+                     
+        public void Recalc(KnotenUc sender)
+        {
+            RecalcBezier(SourceKnotenUc, DestKnotenUc);
+                    
+        }
+        private void RecalcBezier(KnotenUc startElement, KnotenUc endElement)
+        {
+            switch (Type)
+            {
+                case KantenType.Proecess:
+                    if (Canvas.GetTop(SourceKnotenUc) < Canvas.GetTop(DestKnotenUc))
+                    {
+                        StatusSourceElement = DockedStatus.Bottom;
+                        StatusDestElement = DockedStatus.Top;
+                    }
+                    else
+                    {
+                        StatusSourceElement = DockedStatus.Top;
+                        StatusDestElement = DockedStatus.Bottom;
+                    }
+                    break;
+                case KantenType.Recursiv:
+                    StatusSourceElement = DockedStatus.Left;
+                    StatusDestElement = DockedStatus.Left;
+                    break;
+                case KantenType.ReturnProcess:
+                    StatusSourceElement = DockedStatus.Rigth;
+                    StatusDestElement = DockedStatus.Rigth;
+                    break;
+   
+            }
+            int indexStartElement = startElement.GetIndexOfKante(this);
+            int indexDestElement = endElement.GetIndexOfKante(this);
+            int amountIndexStart = startElement.GetAmountOfIndexOfKante(this);
+            int amountIndexEnd = endElement.GetAmountOfIndexOfKante(this);
+
+            Point startpoint,endpoint;
+            switch (Type)
+            {
+                case KantenType.Proecess:
+                    startpoint = new Point(Canvas.GetLeft(SourceKnotenUc) + (SourceKnotenUc.Width / amountIndexStart) * indexStartElement, Canvas.GetTop(SourceKnotenUc));
+                    endpoint = new Point(Canvas.GetLeft(DestKnotenUc) + (DestKnotenUc.Width / amountIndexEnd) * indexDestElement, Canvas.GetTop(DestKnotenUc));
+                    double heightStart, heightEnd;
+                    heightStart = StatusSourceElement == DockedStatus.Bottom ? startElement.Height : 0;
+                    heightEnd = StatusDestElement == DockedStatus.Bottom ? startElement.Height : 0;
+
+                    pthFigure.StartPoint = new Point(startpoint.X, startpoint.Y + heightStart);
+                    bzSeg.Point1 = new Point(startpoint.X, endpoint.Y + heightEnd);
+                    bzSeg.Point2 = new Point(endpoint.X, startpoint.Y + heightStart);
+                    bzSeg.Point3 = new Point(endpoint.X, endpoint.Y + heightEnd); ;   
+                    break;
+                case KantenType.Recursiv:
+                    startpoint = new Point(Canvas.GetLeft(SourceKnotenUc), Canvas.GetTop(SourceKnotenUc)+(SourceKnotenUc.Height/ amountIndexStart) * indexStartElement);
+                    pthFigure.StartPoint = startpoint;
+                    bzSeg.Point1 = new Point(startpoint.X - (SourceKnotenUc.Width/2), startpoint.Y - SourceKnotenUc.Width/2);
+                    bzSeg.Point2 = new Point(startpoint.X - (SourceKnotenUc.Width/2), startpoint.Y + SourceKnotenUc.Width/2);
+                    bzSeg.Point3 = startpoint;
+                    break;
+                case KantenType.ReturnProcess:
+                    startpoint = new Point(Canvas.GetLeft(SourceKnotenUc) + SourceKnotenUc.Width, Canvas.GetTop(SourceKnotenUc) + (SourceKnotenUc.Height / amountIndexStart) * indexStartElement);
+                    endpoint = new Point(Canvas.GetLeft(DestKnotenUc) + DestKnotenUc.Width, Canvas.GetTop(DestKnotenUc) + (DestKnotenUc.Height / amountIndexEnd) * indexDestElement);
+            
+                    pthFigure.StartPoint = startpoint;
+                    bzSeg.Point1 = new Point(startpoint.X + SourceKnotenUc.Width / 2, startpoint.Y);
+                    bzSeg.Point2 = new Point(startpoint.X + DestKnotenUc.Width / 2, endpoint.Y);
+                    bzSeg.Point3 = endpoint;   
+                    break;
+         
+            }
+            myPathSegmentCollection.Clear();
+            myPathSegmentCollection.Add(bzSeg);
+            pthFigure.Segments = myPathSegmentCollection;
+            pthFigureCollection.Clear();
+            pthFigureCollection.Add(pthFigure);
+
+            pthGeometry.Figures = pthFigureCollection;
+
+            ArrowCappedLine.LinePath = pthGeometry;
+       }
+
+        private bool selected;
+
+        public bool Selected
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                if(selected)
+                    ArrowCappedLine.Stroke = new SolidColorBrush(Colors.Orange);
+                else
+                    ArrowCappedLine.Stroke = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        public DockedStatus StatusSourceElement { get; set; }
+        public DockedStatus StatusDestElement { get; set; }
+        public KantenType Type { get; set; }
+        
+        public enum KantenType { Proecess,Recursiv,ReturnProcess }
+
+        public enum DockedStatus { Top,Bottom, Left, Rigth}
+
+
+        private void KanteUc_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Selected = !Selected;
+        }
+    }
+}
---
+++ Basisverzeichnis/trunk/09_Beispiele/Graph zeichnen/GraphDrawing/GraphDrawing/MainWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GraphDrawing.DrawingElements;
+
+namespace GraphDrawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            KnotenUc Kn1 = AddKnoten(1,"1");
+            Kn1.StartKnoten = true;
+            KnotenUc Kn2 = AddKnoten(1, "2");
+            KnotenUc Kn3 = AddKnoten(1, "3");
+            KnotenUc Kn2b1 = AddKnoten(4, "2b1", Kn2);
+            KnotenUc Kn2a1 = AddKnoten(3, "2a1", Kn2);
+            KnotenUc Kn2a2 = AddKnoten(3, "2a2");
+            KnotenUc Kn2a3 = AddKnoten(3, "2a3");
+            KnotenUc Kn4 = AddKnoten(1, "4");
+            KnotenUc Kn3a1 = AddKnoten(2, "3a1", Kn3);
+            AddKante(Kn1, Kn2);
+            AddKante(Kn2, Kn3);
+            AddKante(Kn3, Kn3);
+            AddKante(Kn2, Kn2a1);
+            AddKante(Kn2a1, Kn2a2);
+            AddKante(Kn2a2, Kn2a3);
+            AddKante(Kn2a3, Kn1);
+            AddKante(Kn2, Kn2b1);
+            AddKante(Kn3, Kn4);
+            AddKante(Kn3, Kn3a1);
+            //ReCalcPositionsOfElements();
+        }
+
+        private const double minWidth = 160;
+        private const double minHeight = 150;
+        private List<KnotenUc> KnotenList = new List<KnotenUc>(); 
+
+        public KnotenUc AddKnoten(uint variante, string name,KnotenUc referenceKnotenUc = null)
+        {
+            KnotenUc Knoten = new KnotenUc(variante, name);
+            Knoten.PreviewMouseLeftButtonDown += UIElement_OnMouseDown;
+            DrawingCanvas.Children.Add(Knoten);
+
+            if (referenceKnotenUc != null)
+                Knoten.Offset = referenceKnotenUc.Offset;
+
+            for (int i = 0; i < KnotenList.Count; i++)
+            {
+                if (KnotenList[i].Variante == variante && KnotenList[i].Offset > Knoten.Offset)
+                    Knoten.Offset = KnotenList[i].Offset;
+            }
+            if (KnotenList.Count > 0)
+                Knoten.Offset += minHeight;
+            
+            KnotenList.Add(Knoten);
+            Canvas.SetTop(Knoten, Knoten.Offset);
+            Canvas.SetLeft(Knoten, minWidth*variante);
+
+            
+            return Knoten;
+        }
+
+        //public void ReCalcPositionsOfElements()
+        //{
+        //    KnotenUc searchKnotenUc = KnotenList.Find(uc => uc.StartKnoten);
+        //    if (searchKnotenUc != null)
+        //    {
+        //        CalcPosition(searchKnotenUc, 0);
+        //    }
+        //}
+
+        //private void CalcPosition(KnotenUc source, double offset)
+        //{
+        //    for (int i = 0; i < source.KantenList.Count; i++)
+        //    {
+        //        if (!source.KantenList[i].SourceKnotenUc.Equals(source) && !source.KantenList[i].DestKnotenUc.Equals(source.KantenList[i].SourceKnotenUc))
+        //        {
+        //            source.KantenList[i].DestKnotenUc.Offset = source.Offset + minHeight;
+        //            CalcPosition(source.KantenList[i].DestKnotenUc, source.KantenList[i].DestKnotenUc.Offset);
+        //        }
+        //    }
+        //}
+
+        public KanteUc AddKante(KnotenUc kn1, KnotenUc kn2)
+        {
+            if (kn1 == null || kn2 == null)
+                return null;
+            KanteUc ergKanteUc = new KanteUc(kn1, kn2);
+            DrawingCanvas.Children.Add(ergKanteUc);
+            if (kn1.Variante < kn2.Variante)
+                kn2.Offset = kn1.Offset + minHeight;
+
+            for (int i = 0; i < KnotenList.Count; i++)
+            {
+                KnotenList[i].RecalcKanten();
+            }
+
+            return ergKanteUc;
+        }
+        
+
+        private FrameworkElement selElement;
+        private FrameworkElement otherElement = null;
+        private Point offsetElementPosition;        
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is FrameworkElement)
+            {
+                selElement = (FrameworkElement)sender;
+                offsetElementPosition = Mouse.GetPosition(DrawingCanvas);
+                Point elementPoint = new Point(Canvas.GetLeft(selElement), Canvas.GetTop(selElement));
+                offsetElementPosition.X -= elementPoint.X;
+                offsetElementPosition.Y -= elementPoint.Y;
+            }
+
+        }
+
+        private void UIElement_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (selElement != null)
+            {
+                foreach (KnotenUc fe in DrawingCanvas.Children)
+                {
+                    if(fe != selElement)
+                        continue;
+
+                    Canvas.SetTop(fe, e.GetPosition(this).Y - offsetElementPosition.Y);
+                    Canvas.SetLeft(fe, e.GetPosition(this).X - offsetElementPosition.X);
+                    
+                    fe.RecalcKanten();
+                    selElement = null;
+                    break;
+
+                }
+            }
+        }
+
+        
+    
+
+        private void MainWindow_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (selElement != null)
+            {
+                foreach (FrameworkElement fe in DrawingCanvas.Children)
+                {
+                    if (fe != selElement)
+                        continue;
+
+                    Canvas.SetTop(fe, e.GetPosition(this).Y - offsetElementPosition.Y);
+                    Canvas.SetLeft(fe, e.GetPosition(this).X - offsetElementPosition.X);
+
+                }
+            }
+        }
+    }
+}
