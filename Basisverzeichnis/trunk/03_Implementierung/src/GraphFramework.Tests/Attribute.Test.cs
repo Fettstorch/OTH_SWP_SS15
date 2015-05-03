cb3d8b5359eee4aed0e480728943097d30cdc667
SebMertz <sebastian.mertz@web.de>
2015-05-03 22:45:54 +0200

    Erstellung Testklassen und erste Tests #35

---
+++ Basisverzeichnis/trunk/03_Implementierung/src/GraphFramework.Tests/Attribute.Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GraphFramework.Tests
+{
+    public class Attribute : BaseUnitTest
+    {
+        [TestCase("name", 1, typeof(int))]
+        public void ConstructorTest(string name, object value, System.Type type)
+        {
+            GraphFramework.Attribute TestAttribute = new GraphFramework.Attribute(name, value);
+            Assert.AreEqual(name, TestAttribute.Name);
+            Assert.AreEqual(value, TestAttribute.Value);
+            Assert.AreEqual(type, TestAttribute.Type);
+        }
+
+        [ExpectedException(typeof(System.Exception))]
+        [TestCase("", 1, typeof(int))]
+        [TestCase(null, 1, typeof(int))]
+        [TestCase("name", null, typeof(int))] 
+        public void ConstructorExceptionTest(string name, object value, System.Type type)
+        {
+            GraphFramework.Attribute TestAttribute = new GraphFramework.Attribute(name, value);
+            Assert.AreEqual(name, TestAttribute.Name);
+            Assert.AreEqual(value, TestAttribute.Value);
+            Assert.AreEqual(type, TestAttribute.Type);
+        }
+
+        [TestCase("name", 1, "name", typeof(int), 1)]
+        [TestCase("name", 1, 2, typeof(int), 2)]
+        public void ValueTest(string name, object value, object assignedValue, System.Type type, object expectedValue)
+        {
+            GraphFramework.Attribute TestAttribute = new GraphFramework.Attribute(name, value);
+            TestAttribute.Value = assignedValue;
+            Assert.AreEqual(name, TestAttribute.Name);
+            Assert.AreEqual(expectedValue, TestAttribute.Value);
+            Assert.AreEqual(type, TestAttribute.Type);
+        }
+    }
+}
---
+++ Basisverzeichnis/trunk/03_Implementierung/src/GraphFramework.Tests/BaseUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GraphFramework.Tests
+{
+    [TestFixture]
+    public class BaseUnitTest
+    {
+        [SetUp]
+        public virtual void OnTestStarted()
+        {
+
+        }
+
+        [TearDown]
+        public virtual void OnTestFinished()
+        {
+
+        }
+    }
+}
