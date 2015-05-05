b19371f0c3f43f7d67f48cfdfd71ffd2257cf277
Valdiralita <stefanpoelloth@yahoo.de>
2015-05-05 03:15:13 +0200

    Implemented Logging

---
+++ Basisverzeichnis/trunk/03_Implementierung/src/LogManager/Internal/Logger.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LogManager
+{
+    #region Enums
+
+    public enum LogLevel
+    {
+        Trace,
+        Debug,
+        Error,
+        Exception,
+        Status
+    }
+
+    [Flags]
+    public enum LogTarget
+    {
+        Console = 1,
+        File = 2,
+    }
+
+    public enum LogFileNameType
+    {
+        Date,
+        Rolling
+    }
+
+    #endregion
+
+    class LogManager
+    {
+        string Filename;
+        object lockObject = new object();
+        LogFileNameType FileNameType;
+        LogTarget Target;
+        string LogPath;
+        LogLevel minimumLogLevel;
+
+        public LogManager()
+        {
+            minimumLogLevel = LogLevel.Trace;
+            FileNameType = LogFileNameType.Date;
+            Filename = GetFileName();
+            LogPath = "Logs";
+            Target = LogTarget.Console;
+        }
+
+        public void Log(string message, LogLevel level)
+        {
+            if (level < minimumLogLevel) return;
+
+            var time = DateTime.Now;
+            string stringToLog = String.Format("{0}:{1}:{2}.{3} [{4}] {5}", time.Hour, time.Minute, time.Second, time.Millisecond, level, message);
+
+            if (Target.HasFlag(LogTarget.File))
+            {
+                lock (lockObject)
+                {
+                    if (!File.Exists(Filename)) File.Create(Filename).Close();
+
+                    using (var sw = File.AppendText(Filename))
+                    {
+                        sw.WriteLine(stringToLog);
+                    }
+                }
+            }
+            if (Target.HasFlag(LogTarget.Console))
+            {
+                Console.WriteLine(stringToLog);
+            }
+        }
+
+        private string GetFileName()
+        {
+            if (FileNameType == LogFileNameType.Date)
+            {
+                var time = DateTime.Now;
+                return String.Format("session.{0}.{1}.{2}_{3}.{4}.{5}", time.Day, time.Month, time.Year, time.Hour, time.Minute, time.Second);
+            }
+            else //if (FileNameType == LogFileNameType.Rolling)
+            {
+                string obsoleteFile = Path.Combine(LogPath, "session.9.log");
+                File.Delete(obsoleteFile);
+
+                for (int i = 8; i < 0; i--)
+                {
+                    string oldFilename = String.Format("session.{0}.log", i);
+                    string newFilename = String.Format("session.{0}.log", i + 1);
+
+                    if (File.Exists(oldFilename))
+                    {
+                        File.Move(oldFilename, newFilename);
+                    }
+                }
+
+                return "session.0.log";
+            }
+        }
+
+        internal void SetLogLevel(LogLevel level)
+        {
+            minimumLogLevel = level;
+        }
+    }
+}
---
+++ Basisverzeichnis/trunk/03_Implementierung/src/LogManager/LoggingFunctions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace LogManager
+{
+    static class LoggingFunctions
+    {
+        private static LogManager logger;
+
+        static LoggingFunctions()
+        {
+            logger = new LogManager();
+        }
+
+        public static void Trace(object toLog)
+        {
+            Log(toLog, LogLevel.Trace);
+        }
+
+        public static void Debug(object toLog)
+        {
+            Log(toLog, LogLevel.Debug);
+        }
+
+        public static void Error(object toLog)
+        {
+            Log(toLog, LogLevel.Error);
+        }
+
+        public static void Exception(object toLog)
+        {
+            Log(toLog, LogLevel.Exception);
+        }
+
+        public static void Status(object toLog)
+        {
+            Log(toLog, LogLevel.Status);
+        }
+
+        public static void Log(object toLog, LogLevel level)
+        {
+            if (toLog == null) return;
+
+            string message = toLog.ToString();
+
+            if (toLog is IList || toLog is Array || toLog is IDictionary)
+            {
+                var enumerable = toLog as IEnumerable;
+
+                if (enumerable != null)
+                {
+                    message = String.Empty;
+                    foreach (var part in enumerable)
+                    {
+                        message += String.Format("{0}" + Environment.NewLine, part.ToString());
+                    }
+                }
+            }
+            logger.Log(message, level);    
+        }
+
+        public static void SetLogLevel(LogLevel level)
+        {
+            logger.SetLogLevel(level);
+        }
+
+    }
+}
