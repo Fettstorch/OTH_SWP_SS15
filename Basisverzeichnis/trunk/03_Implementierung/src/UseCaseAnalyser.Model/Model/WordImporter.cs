aec8066ac827e253979d61d4bf7189cd3f5d0127
Ratatoeskr <erik.hegen@t-online.de>
2015-05-06 21:16:30 +0200

    Implementierung WordImporter (WIP)
    Verweise eingefügt, erste Implementierung. Bitte noch nix
    ändern/reviewen, ist noch nicht fertig!
    #41

--- Basisverzeichnis/trunk/03_Implementierung/src/UseCaseAnalyser.Model/Model/WordImporter.cs
+++ Basisverzeichnis/trunk/03_Implementierung/src/UseCaseAnalyser.Model/Model/WordImporter.cs
@@ -1,18 +1,110 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Windows.Forms;
+using LogManager;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using GraphFramework;
+using Table = DocumentFormat.OpenXml.Wordprocessing.Table;
 
 namespace UseCaseAnalyser.Model.Model
 {
     public static class WordImporter
     {
+        private static string[] importStepNames =
+        {
+            "Name",
+            "Kennung",
+            "Priorität",
+            "Kurzbeschreibung:",
+            "Vorbedingung(en):",
+            "Nachbedingung(en):",
+            "Normaler Ablauf:",
+            "Ablauf-Varianten:",
+            "Spezielle Anforderungen:",
+            "Zu klärende Punkte:"
+        };
+
+        private enum ImportStep
+        {
+            Name = 0, 
+            Id,  
+            Priority, 
+            Description, 
+            PreCondition, 
+            PostCondition, 
+            NormalRoutine,
+            SequenceVariation, 
+            SpecialRequirements, 
+            OpenPoints
+        };
+
         /// <summary>
         /// imports the specified file (.docx) to a use case graph
         /// </summary>
         /// <param name="file">file to import</param>
-        /// <returns>the use case graph generated from the file</returns>
-        public static UseCaseGraph[] ImportUseCases(FileInfo file)
+        /// <returns>list of the use case graphs generated from the file</returns>
+        public static List<UseCaseGraph> ImportUseCases(FileInfo file)
         {
-            throw new NotImplementedException();
+            List<UseCaseGraph> useCaseList = new List<UseCaseGraph>();
+
+            if (!File.Exists((file.Name)))
+            {
+                // To-Do: Loggers
+                return useCaseList;
+            }
+            
+            WordprocessingDocument doc;
+            try
+            {
+                doc = WordprocessingDocument.Open(file.Name, false);
+            }
+            catch (Exception ex)
+            {
+                // To-Do: Logger
+                MessageBox.Show(ex.Message);
+                return useCaseList;
+            }
+
+            IEnumerable<Table> tables = doc.MainDocumentPart.Document.Descendants<Table>();
+            int tableNumber = 0;
+            foreach (Table table in tables)
+            {
+                // every possible use case table
+                IEnumerable<TableRow> rows = table.Descendants<TableRow>();
+                UseCaseGraph useCaseGraph;
+                if (WordImporter.TryReadInUseCase(rows, out useCaseGraph))
+                {
+                    useCaseList.Add(useCaseGraph);
+                    tableNumber++;
+                }
+            }
+
+            if (tableNumber == 0)
+            {
+                // Es konnten keine Use Cases gefunden werden. To-Do: Warnung, Logger, etc!
+            }
+
+            return useCaseList; 
+        }
+
+        private static bool TryReadInUseCase(IEnumerable<TableRow> tableRows, out UseCaseGraph useCaseGraph)
+        {
+            useCaseGraph = new UseCaseGraph();
+
+            // To-Do: Auslesen, Validieren, etc. ...to be continued
+            foreach (TableRow row in tableRows)
+            {
+                IEnumerable<Cell> cells = row.Descendants<Cell>();
+                foreach (Cell cell in cells)
+                {
+                    
+                }
+            }
+
+            return false;
         }
     }
 }
