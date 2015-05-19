57b03e961b3c185540d34df9327ce89e62950b78
SebMertz <sebastian.mertz@web.de>
2015-05-19 18:26:56 +0200

    Erste Version Finden von Start- und Endknoten in ScenarioCreator #69

--- Basisverzeichnis/trunk/03_Implementierung/src/UseCaseAnalyser.Model/Model/ScenarioMatrixCreator.cs
+++ Basisverzeichnis/trunk/03_Implementierung/src/UseCaseAnalyser.Model/Model/ScenarioMatrixCreator.cs
@@ -9,6 +9,42 @@ namespace UseCaseAnalyser.Model.Model
 {
     public static class ScenarioMatrixCreator
     {
+        private static INode FindStartingNode(IGraph graph)
+        {          
+            foreach (INode node in graph.Nodes)
+            {
+                foreach (IAttribute attribute in node.Attributes)
+                {
+                    if (!attribute.Name.Equals(
+                        WordImporter.UseCaseNodeAttributeNames[(int) WordImporter.UseCaseNodeAttributes.NodeType]))
+                        continue;
+                    if (attribute.Value.Equals(WordImporter.NodeTypeAttribute.StartNode))
+                    {
+                        return node;
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<INode> FindEndNodes(IGraph graph)
+        {
+            List<INode> endNodes = new List<INode>();
+            foreach (INode node in graph.Nodes)
+            {
+                foreach (IAttribute attribute in node.Attributes)
+                {
+                    if (!attribute.Name.Equals(
+                        WordImporter.UseCaseNodeAttributeNames[(int)WordImporter.UseCaseNodeAttributes.NodeType]))
+                        continue;
+                    if (attribute.Value.Equals(WordImporter.NodeTypeAttribute.EndNode))
+                    {
+                        endNodes.Add(node);
+                    }
+                }
+            }
+            return endNodes;
+        }
+
         /// <summary>
         /// creates the scenarios from a use case graph
         /// </summary>
@@ -16,6 +52,17 @@ namespace UseCaseAnalyser.Model.Model
         /// <returns>scenario matrix (as array of graphs --> scenarios)</returns>
         public static IEnumerable<IGraph> CreateScenarios(UseCaseGraph useCaseGraph)
         {
+            IEnumerable<IGraph> allScenarios = new List<IGraph>();
+
+            INode startNode = FindStartingNode(useCaseGraph);
+
+            IEnumerable<INode> endNodes = FindEndNodes(useCaseGraph);
+
+            foreach (INode endNode in endNodes)
+            {
+                //find all paths from startNode
+            }
+            
             //  EMPTY ENUMERABLE SO THE VIEW CAN BE TESTED WITHOUT CRASHES
             return Enumerable.Empty<IGraph>();
         }
