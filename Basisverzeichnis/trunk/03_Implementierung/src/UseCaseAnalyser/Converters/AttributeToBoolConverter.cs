07754db075c009bb2911146025647fa55743d19c
Parsen07 <pascaldabs@web.de>
2015-05-25 18:21:45 +0200

    file header to missing files #66

--- Basisverzeichnis/trunk/03_Implementierung/src/UseCaseAnalyser/Converters/AttributeToBoolConverter.cs
+++ Basisverzeichnis/trunk/03_Implementierung/src/UseCaseAnalyser/Converters/AttributeToBoolConverter.cs
@@ -1,3 +1,15 @@
+#region Copyright information
+// <summary>
+// <copyright file="AttributeToBoolConverter.cs">Copyright (c) 2015</copyright>
+// 
+// <creationDate>13/05/2015</creationDate>
+// 
+// <professor>Prof. Dr. Kurt Hoffmann</professor>
+// <studyCourse>Angewandte Informatik</studyCourse>
+// <branchOfStudy>Industrieinformatik</branchOfStudy>
+// <subject>Software Projekt</subject>
+// </summary>
+#endregion
 using System;
 using System.Globalization;
 using System.Windows.Data;
