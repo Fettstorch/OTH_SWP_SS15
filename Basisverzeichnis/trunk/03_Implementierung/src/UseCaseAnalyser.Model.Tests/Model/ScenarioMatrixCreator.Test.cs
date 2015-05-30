bd6545f5f6043d450bef7bca96f7b9beba1c105d
SebMertz <sebastian.mertz@web.de>
2015-05-30 10:32:32 +0200

    UnitTests ScenarioMatrixCreator: Erstellung Testklasse #92

---
+++ Basisverzeichnis/trunk/03_Implementierung/src/UseCaseAnalyser.Model.Tests/Model/ScenarioMatrixCreator.Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UseCaseAnalyser.Model.Tests.Model
+{
+    [TestFixture]
+    class ScenarioMatrixCreator
+    {
+        [SetUp]
+        public virtual void OnTestStarted()
+        {
+
+        }
+
+        [TearDown]
+        public virtual void OnTestFinished()
+        {
+
+        }
+    }
+}
