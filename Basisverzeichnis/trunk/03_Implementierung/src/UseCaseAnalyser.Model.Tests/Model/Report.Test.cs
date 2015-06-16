c224bfdf77294d5ea69ffc6dec1401812c24b60e
SebMertz <sebastian.mertz@web.de>
2015-06-16 12:50:22 +0200

    Report Tests: Dummy Implementierung #100

---
+++ Basisverzeichnis/trunk/03_Implementierung/src/UseCaseAnalyser.Model.Tests/Model/Report.Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UseCaseAnalyser.Model.Tests.Model
+{
+    [TestFixture]
+    class ReportTests
+    {
+        [SetUp]
+        public virtual void OnTestStarted()
+        {
+        }
+
+        [TearDown]
+        public virtual void OnTestFinished()
+        {
+
+        }
+    }
+}
